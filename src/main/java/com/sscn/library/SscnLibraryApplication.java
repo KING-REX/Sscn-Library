5069ef71c2c187b50bcbf24777410d0712871a7e
King Rex <anijimjamesmaurice@gmail.com>
2023-08-11 15:18:50 +0100

    Rex's first Commit

---
+++ src/main/java/com/sscn/library/SscnLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.sscn.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SscnLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SscnLibraryApplication.class, args);
+	}
+
+}
