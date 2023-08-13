cc63bc04819414f9aa22ba40978835a7f14e33a3
NoahV <noahvikoo3@gmail.com>
2023-08-13 20:20:41 +0100

    Noah's Commit

--- src/main/java/com/sscn/library/entity/Member.java
+++ src/main/java/com/sscn/library/entity/Member.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UniqueElements;
 
 import java.io.Serializable;
 
--- src/main/java/com/sscn/library/service/MemberService.java
+++ src/main/java/com/sscn/library/service/MemberService.java
@@ -1,10 +1,12 @@
 package com.sscn.library.service;
 
 import com.sscn.library.entity.Member;
+import com.sscn.library.exception.DuplicateValueException;
 import com.sscn.library.exception.NotFoundException;
 import com.sscn.library.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
 
 @Service
@@ -19,12 +21,18 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Member addNewMember(Member member){
+    public Member addNewMember(Member member)  {
+        Member savedMember = memberRepository.findByMail(member.getEmail());
+        if(savedMember != null){
+            throw new RuntimeException("Member with email " + savedMember.getEmail() + "already exists");
+        }
         return memberRepository.save(member);
     }
 
     public List<Member> addMembers(List<Member> members){
-        members.forEach(member -> this.addNewMember(member));
+        members.forEach(member -> {
+            this.addNewMember(member);
+        });
         return members;
     }
 
@@ -34,5 +42,15 @@ public class MemberService {
 
     public Member getMemberById(Integer id){
         return memberRepository.findById(id).orElseThrow(() -> new NotFoundException("Member is not found".formatted(id)));
+    }
+
+    public Member getMemberByEmail(String email){
+        Member member = memberRepository.findByMail(email);
+        if(member != null){
+            return member;
+        }
+        else{
+            throw new NotFoundException("Email Not Found: " + email);
+        }
     }
 }
